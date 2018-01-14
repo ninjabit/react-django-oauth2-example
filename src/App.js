17cf735039560f546d0fa477dfc846fbd6619c6b
Borislav Hadzhiev <opmftw@abv.bg>
2018-01-14 20:58:49 +0200

    Login/Logout buttons with  gapi initialization in ComponentWillMount lcm

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+import GoogleButtons from "./GoogleButtons";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Google Login and Logout Buttons</h1>
+        </header>
+        <GoogleButtons />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/GoogleButtons.js
@@ -0,0 +1,42 @@
+/* global gapi */
+import React from "react";
+
+import { GoogleLogin, GoogleLogout } from "react-google-login";
+
+class GoogleButtons extends React.Component {
+    componentWillMount() {
+        gapi.load("auth2", () => {
+            this.auth2 = gapi.auth2.init({
+                client_id:
+                    "254472747355-6umtrkcedqn00tg7ec17l705ftttam0r.apps.googleusercontent.com"
+            });
+        });
+    }
+
+    render() {
+        const responseGoogle = response => {
+            console.log(response);
+        };
+
+        function signOut() {
+            var auth2 = gapi.auth2.getAuthInstance();
+            auth2.signOut().then(function() {
+                console.log("User signed out.");
+            });
+        }
+
+        return (
+            <div>
+                <GoogleLogin
+                    clientId="254472747355-6umtrkcedqn00tg7ec17l705ftttam0r.apps.googleusercontent.com"
+                    buttonText="Login"
+                    onSuccess={responseGoogle}
+                    onFailure={responseGoogle}
+                />
+                <GoogleLogout buttonText="Logout" onLogoutSuccess={signOut} />
+            </div>
+        );
+    }
+}
+
+export default GoogleButtons;
