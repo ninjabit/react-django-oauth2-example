13afa83484954b415263838d8169d818e10d2cc7
Borislav Hadzhiev <opmftw@abv.bg>
2018-01-19 16:04:51 +0200

    added the github code actions

---
+++ src/actions/githubCodeActions.js
@@ -0,0 +1,39 @@
+const url = "http://127.0.0.1:8000";
+
+const isSendingGithubCode = () => ({
+  type: "SENDING_GITHUB_CODE"
+});
+
+const sentGithubCodeSuccess = json => ({
+  type: "SENT_GITHUB_CODE_SUCCESS",
+  github_token: json
+});
+
+const sentGithubCodeFailure = err => ({
+  type: "SENT_GITHUB_CODE_FAILURE",
+  err
+});
+
+function sendGithubCode(code) {
+  return async function(dispatch) {
+    dispatch(isSendingGithubCode());
+    try {
+      let response = fetch(`${url}/github/`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application-json"
+        },
+        body: JSON.stringify(code)
+      });
+      if (!response.ok) {
+        throw new Error("Invalid authorization with Github. Please try again.");
+      }
+      let responseJson = response.json();
+      return dispatch(sentGithubCodeSuccess(responseJson));
+    } catch (err) {
+      return dispatch(sentGithubCodeFailure(err));
+    }
+  };
+}
+
+export { sendGithubCode };
--- src/components/GithubLoginButton.js
+++ src/components/GithubLoginButton.js
@@ -7,28 +7,6 @@ class GithubLoginButton extends Component {
       console.log(response);
     };
     const onFailure = response => console.error(response);
-
-    // const responseGoogle = response => {
-    // 	console.log(response);
-    // 	this.props
-    // 		.sendGoogleData({
-    // 			token: response.tokenId,
-    // 			email: response.profileObj.email,
-    // 			familyName: response.profileObj.familyName,
-    // 			givenName: response.profileObj.givenName,
-    // 			googleId: response.profileObj.googleId,
-    // 			imageUrl: response.profileObj.imageUrl,
-    // 			name: response.profileObj.name
-    // 		})
-    // 		.then(
-    // 			(
-    // 				history = this.props.history,
-    // 				dispatch = this.props.dispatch
-    // 			) => {
-    // 				this.props.authenticateAction(history, dispatch);
-    // 			}
-    // 		);
-
     return (
       <GitHubLogin
         clientId="cef1ec48f4ae7d49fcf0"
