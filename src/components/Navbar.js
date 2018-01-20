ad760bfbff5c323d8d1a49c77b43d5ac656cde77
Borislav Hadzhiev <opmftw@abv.bg>
2018-01-20 16:58:43 +0200

    reformed the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { NavLink, withRouter } from "react-router-dom";
 
 import GoogleLoginButton from "../containers/GoogleLoginButtonContainer.js";
@@ -9,37 +9,34 @@ import GithubLogoutButton from "../containers/GithubLogoutButtonContainer";
 
 import "../index.css";
 
-class Navbar extends Component {
-  googNavbarLinks() {
-    if (this.props.goog_auth.isAuthenticated) {
+function Navbar(props) {
+  function userIsAuthenticatedGoogle() {
+    if (props.goog_auth.isAuthenticated) {
       return [
         <li className="nav-item" key="goog-logout-btn">
-          <GoogleLogoutButton history={this.props.history} />
+          <GoogleLogoutButton history={props.history} />
         </li>
       ];
     }
   }
-  loginNavbarButtons() {
+  function userIsNotAuthenticated() {
     if (
-      !this.props.goog_auth.isAuthenticated &&
-      !this.props.github_auth.isAuthenticated
+      !props.goog_auth.isAuthenticated &&
+      !props.github_auth.isAuthenticated
     ) {
       return [
         <li className="nav-item" key="goog-login-btn">
-          <GoogleLoginButton history={this.props.history} />
+          <GoogleLoginButton history={props.history} />
         </li>,
-        <li className="tryme nav-item" key="git-login-btn">
+        <li className="nav-item" key="git-login-btn">
           <GithubLoginButton />
         </li>
       ];
     }
   }
 
-  secretNavbarButton() {
-    if (
-      this.props.goog_auth.isAuthenticated |
-      this.props.github_auth.isAuthenticated
-    ) {
+  function userIsAuthenticated() {
+    if (props.goog_auth.isAuthenticated | props.github_auth.isAuthenticated) {
       return [
         <li className="nav-item" key="goog-secret">
           <NavLink to="/secret/" exact className="nav-link">
@@ -50,64 +47,31 @@ class Navbar extends Component {
     }
   }
 
-  githubNavbarLinks() {
-    if (this.props.github_auth.isAuthenticated) {
+  function userIsAuthenticategGithub() {
+    if (props.github_auth.isAuthenticated) {
       return [
         <li className="nav-item mr-3" key="logout-btn">
-          <GithubLogoutButton history={this.props.history} />
+          <GithubLogoutButton history={props.history} />
         </li>
       ];
     }
   }
 
-  render() {
-    return (
-      <nav className="navbar navbar-toggleable-md navbar-light bg-faded">
-        <button
-          className="navbar-toggler navbar-toggler-right"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ">
-            <li className="nav-item active">
-              <NavLink className="nav-link" to="/">
-                Home
-              </NavLink>
-            </li>
-            {this.googNavbarLinks()}
-            {this.githubNavbarLinks()}
-            {this.loginNavbarButtons()}
-            {this.secretNavbarButton()}
-          </ul>
-          {localStorage.getItem("goog_avatar_url") && (
-            <ul className="navbar-nav ml-auto">
-              <li className="avatar-li nav-item">
-                <img
-                  src={localStorage.getItem("goog_avatar_url")}
-                  alt="profile-avatar"
-                  className="avatar-a img-fluid rounded-circle"
-                  title={
-                    "Profile: " +
-                    localStorage.getItem("goog_name") +
-                    " (" +
-                    localStorage.getItem("goog_email") +
-                    ")"
-                  }
-                />
-              </li>
-            </ul>
-          )}
-        </div>
-      </nav>
-    );
-  }
+  return (
+    <nav className="navbar navbar-toggleable-md navbar-light bg-faded">
+      <ul className="navbar-nav ">
+        <li className="nav-item active">
+          <NavLink className="nav-link" to="/">
+            Home
+          </NavLink>
+        </li>
+        {userIsAuthenticatedGoogle()}
+        {userIsAuthenticategGithub()}
+        {userIsNotAuthenticated()}
+        {userIsAuthenticated()}
+      </ul>
+    </nav>
+  );
 }
 
 export default withRouter(Navbar);
